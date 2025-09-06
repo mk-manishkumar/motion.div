6ccffe4096135959efcc63873de9d149283adb59
Manish Kumar <manish.login01@gmail.com>
2025-09-06 23:56:28 +0530

    new :  hover & exit in motion.div

---
+++ chap_02 HoverExit/src/App.tsx
@@ -0,0 +1,11 @@
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-50">
+      <Card />
+    </div>
+  );
+};
+
+export default App;
